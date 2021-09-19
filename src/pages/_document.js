37b4f9071a0f5c6eda99b584be27b4dd1cc95440
Francesco <frasta85@gmail.com>
2021-09-20 00:17:50 +0200

    [BLOG] Favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,18 @@ export default class MyDocument extends Document {
         // const pageProps = _.get(this.props, '__NEXT_DATA__.props.pageProps');
         return (
             <Html {...this.helmetHtmlAttrComponents}>
-                <Head>{this.helmetHeadComponents}</Head>
+                <Head>
+                    {this.helmetHeadComponents}
+                    <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png" />
+                    <link rel="manifest" href="images/favicon/site.webmanifest" />
+                    <link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+                    <meta name="msapplication-TileColor" content="#5bbad5" />
+                    <meta name="theme-color" content="#ffffff" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                </Head>
                 <body {...this.helmetBodyAttrComponents}>
                     <Main />
                     <script src="/js/plugins.js" />
