b3615077731f8c9784e78c5a20ce6423387ac902
Francesco <frasta85@gmail.com>
2021-09-19 18:45:52 +0200

    [BLOG] Header

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -1,14 +1,26 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Box, Flex, Text, IconButton, Stack, useBreakpointValue, useDisclosure } from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { CloseIcon } from '@chakra-ui/icons';
 import MobileMenu from './MobileMenu';
 import Logo from './Logo';
 import Menu from './Menu';
 import LoggedIn from './LoggedIn';
 import LoggedOut from './LoggedOut';
+import _ from 'lodash';
+import { FiMenu } from 'react-icons/fi';
+import { getPageUrl } from '../utils';
 
 const Header = ({ page, config }) => {
     const { isOpen, onClose, onToggle } = useDisclosure();
+    const configTitle = _.get(config, 'title');
+    const header = _.get(config, 'header');
+    const hasNav = _.get(header, 'has_nav');
+    const navLinks = _.get(header, 'nav_links');
+    const logoImage = _.get(header, 'logo_img');
+    const logoImageAlt = _.get(header, 'logo_img_alt');
+    const pageTemplate = _.get(page, 'template');
+    const pageUrl = _.trim(getPageUrl(page), '/');
+    console.log(navLinks, hasNav);
 
     return (
         <Box pos="sticky" top="0" zIndex={1000} shadow="small">
@@ -23,28 +35,29 @@ const Header = ({ page, config }) => {
                 borderColor={'gray.200'}
                 align={'center'}
             >
-                <Flex flex={{ base: 1, lg: 'auto' }} ml={{ base: -2 }} display={{ base: 'flex', lg: 'none' }}>
-                    <IconButton
-                        onClick={onToggle}
-                        icon={isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />}
-                        variant={'ghost'}
-                        aria-label={'Toggle Navigation'}
-                    />
-                    <MobileMenu onClose={onClose} isOpen={isOpen} menu={[]} />
-                </Flex>
-                <Flex flex={{ base: 1 }} justify={{ base: 'center', lg: 'start' }}>
+                <Flex flex={{ base: 1 }} justify={{ base: 'start', lg: 'start' }}>
                     <Text textAlign={useBreakpointValue({ base: 'center', lg: 'left' })} fontFamily={'heading'} color={'gray.800'}>
                         <Logo config={config} />
                     </Text>
 
                     <Flex display={{ base: 'none', lg: 'flex' }} alignItems="center" justifyContent="center" ml={10} flex="1">
-                        <Menu menu={[]} />
+                        {hasNav && !_.isEmpty(navLinks) && <Menu navLinks={navLinks} pageUrl={pageUrl} />}
                     </Flex>
                 </Flex>
 
                 <Stack flex={{ base: 1, lg: 0.3 }} justify={'flex-end'} alignItems="center" direction={'row'} spacing={3}>
                     <LoggedIn /> <LoggedOut />
                 </Stack>
+                <Flex flex={{ base: 1, lg: 'auto' }} justifyContent="flex-end" ml={{ base: -2 }} display={{ base: 'flex', lg: 'none' }}>
+                    <IconButton
+                        color="gray.900"
+                        onClick={onToggle}
+                        icon={isOpen ? <CloseIcon w={3} h={3} /> : <FiMenu />}
+                        variant={'ghost'}
+                        aria-label={'Toggle Navigation'}
+                    />
+                    <MobileMenu onClose={onClose} isOpen={isOpen} menu={[]} />
+                </Flex>
             </Flex>
         </Box>
     );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme/theme';
+import '@fontsource/montserrat/latin.css';
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     return (
