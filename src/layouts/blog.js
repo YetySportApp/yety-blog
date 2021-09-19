941cb7ccf47bdaa0065c75c89ae22f064a3cce1e
corvallo <frasta85@gmail.com>
2021-09-19 13:21:18 +0200

    [BLOG] Header

--- src/layouts/blog.js
+++ src/layouts/blog.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import _ from 'lodash';
-import { Button } from '@chakra-ui/react';
 
 import { Layout } from '../components';
 import { Link, getPageUrl, withPrefix } from '../utils';
@@ -16,7 +15,6 @@ export default class Blog extends React.Component {
 
         return (
             <article key={index} className="cell post">
-                <Button>OK</Button>
                 <div className="card">
                     {thumbImage && (
                         <Link className="post-thumbnail" href={postUrl}>
