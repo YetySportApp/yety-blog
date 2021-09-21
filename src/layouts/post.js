6e1973b3a088024c67df61da89ecc2562191f9b2
Francesco <frasta85@gmail.com>
2021-09-21 16:33:16 +0200

    [BLOG] Fix layout

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
-
 import { Layout } from '../components/index';
-import { htmlToReact, withPrefix, markdownify } from '../utils';
-import BlogPostFooter from '../components/BlogPostFooter';
-import Router from 'next/router';
 import { Container } from '@chakra-ui/react';
 import PostDetails from '../components/PostDetails';
 class Post extends React.Component {
@@ -17,24 +13,6 @@ class Post extends React.Component {
             <Layout page={page} config={config}>
                 <Container maxW="container.xl" h="auto" minH="100vh" p={8}>
                     <PostDetails post={page} data={data} />
-                    {/* <button onClick={() => Router.back()}>as</button>
-                    <div className="outer">
-                        <div className="inner-medium">
-                            <article className="post post-full">
-                                <header className="post-header">
-                                    <h1 className="post-title">{title}</h1>
-                                    {subtitle && <div className="post-subtitle">{htmlToReact(subtitle)}</div>}
-                                </header>
-                                {image && (
-                                    <div className="post-image">
-                                        <img src={withPrefix(image)} alt={imageAlt} />
-                                    </div>
-                                )}
-                                {markdownContent && <div className="post-content">{markdownify(markdownContent)}</div>}
-                                <BlogPostFooter post={page} dateType={'long'} data={data} />
-                            </article>
-                        </div>
-                    </div> */}
                 </Container>
             </Layout>
         );
