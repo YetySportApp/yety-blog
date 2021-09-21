1d6bf9a6569a4606a259faf6b7d4ecb326320edb
Francesco <frasta85@gmail.com>
2021-09-21 16:24:59 +0200

    [BLOG] Fix blog layout

--- src/utils/htmlToChakra.js
+++ src/utils/htmlToChakra.js
@@ -8,42 +8,41 @@ const htmlToChakra = (html) => {
     }
     return ReactHtmlParser(html, {
         transform: (node, index) => {
-            console.log(node);
             if (node.type === 'tag') {
                 switch (node.name) {
                     case 'h1':
                         return (
-                            <Heading as={`${node.name}`} size="xl" py={4}>
+                            <Heading as={`${node.name}`} size="xl" py={4} color="brand.400">
                                 {node.children[0].data}
                             </Heading>
                         );
                     case 'h2':
                         return (
-                            <Heading as={`${node.name}`} size="lg" py={4}>
+                            <Heading as={`${node.name}`} size="lg" py={4} color="brand.400">
                                 {node.children[0].data}
                             </Heading>
                         );
                     case 'h3':
                         return (
-                            <Heading as={`${node.name}`} size="md" py={4}>
+                            <Heading as={`${node.name}`} size="md" py={4} color="brand.400">
                                 {node.children[0].data}
                             </Heading>
                         );
                     case 'h4':
                         return (
-                            <Heading as={`${node.name}`} size="md" fontSize="18px" py={4}>
+                            <Heading as={`${node.name}`} size="md" fontSize="18px" py={4} color="brand.400">
                                 {node.children[0].data}
                             </Heading>
                         );
                     case 'h5':
                         return (
-                            <Heading as={`${node.name}`} size="sm" py={4}>
+                            <Heading as={`${node.name}`} size="sm" py={4} color="brand.400">
                                 {node.children[0].data}
                             </Heading>
                         );
                     case 'h6':
                         return (
-                            <Heading as={`${node.name}`} size="xs" py={4}>
+                            <Heading as={`${node.name}`} size="xs" py={4} color="brand.400">
                                 {node.children[0].data}
                             </Heading>
                         );
