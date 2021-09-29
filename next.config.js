6155a8e1b100455de9fb4b05b2973ff508e429e2
Francesco <frasta85@gmail.com>
2021-09-29 22:03:27 +0200

    [BLOG] master config

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ sourcebit.fetch(sourcebitConfig);
 
 module.exports = (phase) => {
     const env = {
-        PUBLIC_URL: process.env.PUBLIC_URL || 'http://localhost:3006',
-        UPLOAD_ENDPOINT: process.env.UPLOAD_ENDPOINT || 'http://localhost:1337',
-        GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT || 'http://localhost:1337/graphql'
+        PUBLIC_URL: 'https://www.yety.it',
+        UPLOAD_ENDPOINT: 'https://www.yety.it',
+        GRAPHQL_ENDPOINT: 'https://www.yety.it/graphql'
     };
     return {
         env,
