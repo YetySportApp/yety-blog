131aa76778f8392de4b3e732a0faf9c3c276e80e
Francesco <frasta85@gmail.com>
2021-09-29 15:04:15 +0200

    [BLOG] env

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,8 @@
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 const config = {
-    graphqlEndpoint: process.env.GRAPHQL_ENDPOINT,
-    uploadsEndpoint: process.env.UPLOAD_ENDPOINT,
-    publicUrl: process.env.PUBLIC_URL
+    graphqlEndpoint: publicRuntimeConfig.GRAPHQL_ENDPOINT,
+    uploadsEndpoint: publicRuntimeConfig.UPLOAD_ENDPOINT,
+    publicUrl: publicRuntimeConfig.PUBLIC_URL
 };
 export default config;
