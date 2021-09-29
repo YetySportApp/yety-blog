99474d4cce5ef92fbc33524d65400f950d96be15
Francesco <frasta85@gmail.com>
2021-09-29 22:08:04 +0200

    [BLOG] Master

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,15 +2,15 @@ import React from 'react';
 import _ from 'lodash';
 import { Img } from '@chakra-ui/react';
 import { Link, withPrefix } from '../utils';
-import config from '../config/config';
+import cfg from '../config/config';
 
 const Logo = ({ config }) => {
     const header = _.get(config, 'header');
     const logoImage = _.get(header, 'logo_img');
     const logoImageAlt = _.get(header, 'logo_img_alt');
-    console.log(config);
+
     return (
-        <Link href={`${config.publicUrl}`}>
+        <Link href={`${cfg.publicUrl}`}>
             <Img w="150px" h="50px" objectFit="contain" src={logoImage} alt={logoImageAlt} _hover={{ cursor: 'pointer' }} />
         </Link>
     );
