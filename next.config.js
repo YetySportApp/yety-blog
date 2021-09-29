9a45f3672240aaec7c3b139855f7f5498eae680d
Francesco <frasta85@gmail.com>
2021-09-29 14:59:22 +0200

    [BLOG]env

--- next.config.js
+++ next.config.js
@@ -11,10 +11,26 @@ module.exports = (phase) => {
         UPLOAD_ENDPOINT: process.env.UPLOAD_ENDPOINT,
         GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT
     };
-
+    console.log(env);
     console.log(`process.env.PUBLIC_URL ${process.env.PUBLIC_URL}`);
     console.log(`process.env.UPLOAD_ENDPOINT ${process.env.UPLOAD_ENDPOINT}`);
     console.log(`process.env.GRAPHQL_ENDPOINT ${process.env.GRAPHQL_ENDPOINT}`);
+    console.log({
+        env,
+        trailingSlash: true,
+        devIndicators: {
+            autoPrerender: false
+        },
+        webpack: (config, { webpack }) => {
+            // Tell webpack to ignore watching content files in the content folder.
+            // Otherwise webpack receompiles the app and refreshes the whole page.
+            // Instead, the src/pages/[...slug].js uses the "withRemoteDataUpdates"
+            // function to update the content on the page without refreshing the
+            // whole page
+            config.plugins.push(new webpack.WatchIgnorePlugin([[/\/content\//]]));
+            return config;
+        }
+    });
     return {
         env,
         trailingSlash: true,
